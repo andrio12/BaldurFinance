06d74229de3fe0e19a886462bb17fe82e8a0a7e8
Andri Olafsson <andriolafsson@gmail.com>
2022-07-04 12:07:24 +0000

    Removed history

---
+++ Data/StockMarket.cs
@@ -0,0 +1,26 @@
+﻿namespace BaldurFinance.Data
+{
+    public class StockMarket
+    {
+        public class Stock
+        {
+            public Info? Info { get; set; }
+            public Price? Price { get; set; }
+        }
+
+        public class Info
+        {
+            public string? Type { get; set; }
+            public string? Title { get; set; }
+            public string? Ticker { get; set; }
+
+        }
+
+        public class Price
+        {
+            public string? Currency { get; set; }
+            public float PreviousClose { get; set; }
+
+        }
+    }
+}
---
+++ Data/StockMarketService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace BaldurFinance.Data
+{
+	public class StockMarketService
+	{
+		public async Task<List<StockMarket.Stock>> GetStockMarketData()
+        {
+			HttpClient client = new HttpClient();
+			HttpRequestMessage request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://google-finance4.p.rapidapi.com/search/?q=airbnb&hl=en&gl=US")
+			};
+
+			using var response = await client.SendAsync(request);
+			response.EnsureSuccessStatusCode();
+			string body = await response.Content.ReadAsStringAsync();
+			
+			List<StockMarket.Stock> stocks = new List<StockMarket.Stock>();
+
+			JsonSerializer serializer = new JsonSerializer();
+			using (var sr = new StringReader(body))
+			using (var reader = new JsonTextReader(sr))
+			{
+				stocks = serializer.Deserialize<List<StockMarket.Stock>>(reader);
+			}
+
+			return stocks ?? new List<StockMarket.Stock>();
+        }
+
+    }
+}
